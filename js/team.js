5d7942b1ddc1539c0c4738c721f09aa16b094c9f
Rishabh Jain <62394291+r-rishabh-j@users.noreply.github.com>
2021-07-03 15:23:59 +0530

    emails added

--- js/team.js
+++ js/team.js
@@ -1,35 +1,22 @@
 $(document).ready(function(){
-    $("#phone1").hide();
+    // $("#phone1").hide();
     $("#email1").hide();
-    $("#phone2").hide();
+    // $("#phone2").hide();
     $("#email2").hide();
-    $("#phone3").hide();
+    // $("#phone3").hide();
     $("#email3").hide();
-    $("#phone4").hide();
+    // $("#phone4").hide();
     $("#email4").hide();
-    $("#phone5").hide();
+    // $("#phone5").hide();
     $("#email5").hide();
-    $("#phone6").hide();
+    // $("#phone6").hide();
     $("#email6").hide();
-
-    $("#iphone1").click(function(){
-        $("#phone1").toggle();
-    });
-    $("#iphone2").click(function(){
-        $("#phone2").toggle();
-    });
-    $("#iphone3").click(function(){
-        $("#phone3").toggle();
-    });
-    $("#iphone4").click(function(){
-        $("#phone4").toggle();
-    });
-    $("#iphone5").click(function(){
-        $("#phone5").toggle();
-    });
-    $("#iphone6").click(function(){
-        $("#phone6").toggle();
-    });
+    $("#email7").hide();
+    $("#email8").hide();
+    $("#email9").hide();
+    $("#email10").hide();
+    $("#email11").hide();
+    $("#email12").hide();
 
     $("#iemail1").click(function(){
         $("#email1").toggle();
@@ -49,4 +36,42 @@ $(document).ready(function(){
     $("#iemail6").click(function(){
         $("#email6").toggle();
     });
+    $("#iemail7").click(function(){
+        $("#email7").toggle();
+    });
+    $("#iemail8").click(function(){
+        $("#email8").toggle();
+    });
+    $("#iemail9").click(function(){
+        $("#email9").toggle();
+    });
+    $("#iemail10").click(function(){
+        $("#email10").toggle();
+    });
+    $("#iemail11").click(function(){
+        $("#email11").toggle();
+    });
+    $("#iemail11").click(function(){
+        $("#email12").toggle();
+    });
+
+    /*Phone toggles, removed */
+    // $("#iphone1").click(function(){
+    //     $("#phone1").toggle();
+    // });
+    // $("#iphone2").click(function(){
+    //     $("#phone2").toggle();
+    // });
+    // $("#iphone3").click(function(){
+    //     $("#phone3").toggle();
+    // });
+    // $("#iphone4").click(function(){
+    //     $("#phone4").toggle();
+    // });
+    // $("#iphone5").click(function(){
+    //     $("#phone5").toggle();
+    // });
+    // $("#iphone6").click(function(){
+    //     $("#phone6").toggle();
+    // });
 });
